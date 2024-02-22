53599c1c1f85ca24a385b75b91295999fb3d77b6
jamiegallagher <G00377566@gmit.ie>
2024-02-22 15:51:29 +0000

    minor changes to app.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <h1>Welcome to IrishHospitalityHub</h1>
-      </header>
     </div>
   );
 }
